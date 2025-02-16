7086bd8118d083e4d207581914411635406c60e1
Shardendu777 <khalishardendu77@gmail.com>
2025-02-16 12:30:18 +0530

    Add files via upload

---
+++ Armstrong/armstrong1.py
@@ -0,0 +1,39 @@
+#armstrong
+import sys
+n=input()
+try:
+    n=int(n)
+    
+except ValueError:
+    try :
+        n=float(n)
+        print("invalid")
+        raise SystemExit("Exiting the program.")
+
+    except ValueError:
+        try:
+            n=complex(n)
+            print("invalid")
+            raise SystemExit("Exiting the program.")
+
+        except ValueError:
+            print("invalid")
+            raise SystemExit("Exiting the program.")
+
+original_n=n
+count=0
+arm=0
+while(n>0):
+    n=n//10
+    count=count+1
+    
+n=original_n
+
+while(n>0):
+    t=n%10
+    arm+=t**count
+    n=n//10
+if(arm==original_n):
+    print("armstrong")
+else:
+    print("not armstrong")
